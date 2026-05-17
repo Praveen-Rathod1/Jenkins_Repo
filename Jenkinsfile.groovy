91b3f317f69bcad6cb9c0ff493bcf2580f68e9ee
Praveen Rathod <praveensr471@gmail.com>
2026-05-17 22:00:25 +0530

    Add files via upload
    added jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages ( 'Build' ) {
+    steps {
+        sh 'sleep 5'
+        echo " this is build stage "
+    }
+}
+stages ( 'Test' ) {
+steps {
+    sh 'sleep 5'
+    echo " this is test stage "
+}
+}
+stages ( 'Deploy') {
+    steps {
+        sh 'sleep 5'
+        echo " this is deploy stage "
+    }
+}
+}
